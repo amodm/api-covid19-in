b230ab582d5493f19095b7c0ee11eb26693cf984
Amod Malviya <amod@rootnet.in>
2020-03-21 17:40:05 +0530

    Temp hack for location name changes by mohfw

--- index.js
+++ index.js
@@ -18,12 +18,17 @@ async function notFoundHandler() {
   return new Response('404 nothing to see here', { status: 404, statusText: 'Not Found' })
 }
 
-async function cachedData(key) {
+async function cachedData(key, applyLocHacks = false) {
   try {
-    return rawResponse(await Store.get(key));
+    const data= await Store.get(key);
+    return rawResponse(applyLocHacks ? fixLocationNameChanges(data) : data);
   } catch (err) {
     return errorResponse({ "message": err })
   }
+}
+
+function fixLocationNameChanges(content) {
+  return content.replace(/Pondicherry/g, "Puducherry").replace(/Union Territory of /g, "");
 }
 
 const ROUTE_PREFIX = "/covid19-in";
@@ -31,8 +36,8 @@ const ROUTE_PREFIX = "/covid19-in";
 const routeHandlers = {
   '/contacts': () => cachedData(STORE_KEYS.CACHED_CONTACTS),
   '/stats': () => cachedData(STORE_KEYS.CACHED_CASE_COUNTS),
-  '/stats/latest': () => cachedData(STORE_KEYS.CACHED_CASE_COUNTS),
-  '/stats/daily': () => cachedData(STORE_KEYS.CACHED_CASE_COUNTS_HISTORY),
+  '/stats/latest': () => cachedData(STORE_KEYS.CACHED_CASE_COUNTS, true),
+  '/stats/daily': () => cachedData(STORE_KEYS.CACHED_CASE_COUNTS_HISTORY, true),
   '/stats/hospitals': () => cachedData(STORE_KEYS.CACHED_HOSPITAL_BEDS_COUNT),
   '/notifications': () => cachedData(STORE_KEYS.CACHED_NOTIFICATIONS),
   '/unofficial/sources': getAllUnofficialSources,
